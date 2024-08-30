7bc40caa9d67fa77a565dcdf0c36a1696b9201fd
Matthew Connley <matt@mattconnley.com>
2024-08-30 19:41:26 +0000

    Revert "update gitignore, exlude cluster desired-state-spec.json"
    This reverts commit baa916ed9f425eadf6629770add41b142a1673ea.

--- BuildESXiImage.ps1
+++ BuildESXiImage.ps1
@@ -1,21 +1,15 @@
+Connect-VIServer -Server vc8.mattconnley.com -user administrator@vsphere.local
 $datacenterName = "HomeLab"
 $clusterName = "ImageBuildOnly"
 $esxiImageName = "8.0 U3 - 24022510"
 $esxiComponentName = "VMware USB NIC Fling Driver"
-$esxiComponentVersion = "1.14-2vmw"
+$esxiComponentVersion = "1.14-1vmw"
 
-$vc = $args[0]
-$certUrl = "https://$($vc)/certs/download.zip"
-Invoke-WebRequest $certUrl -OutFile "/tmp/download.zip" -SkipCertificateCheck
-Expand-Archive -Path "/tmp/download.zip" -DestinationPath "/tmp"
-Get-ChildItem -Path "/tmp/certs/lin/" -Filter "*.0" | Select-Object -First 1 | Copy-Item -Destination "/usr/local/share/ca-certificates/vc.crt"
-update-ca-certificates
 
-Connect-VIServer -Server $vc
 $esxiBaseImage = Get-LcmImage -Type BaseImage -Version $esxiImageName
 $esxiComponent = Get-LcmImage -Type Component | Where-Object {$_.Name -eq $esxiComponentName -and $_.Version -eq $esxiComponentVersion}
 
 New-Cluster -Name $clusterName -BaseImage $esxiBaseImage -Location (Get-Datacenter -Name $datacenterName)
 Get-Cluster -Name $clusterName | Set-Cluster -Component @($esxiComponent) -BaseImage $esxiBaseImage -Confirm:$false
-Export-LcmClusterDesiredState -Cluster (Get-Cluster -Name $clusterName) -ExportIsoImage -Destination "/tmp/imagebuilds"
+Export-LcmClusterDesiredState -Cluster (Get-Cluster -Name $clusterName) -ExportIsoImage
 Get-Cluster -Name $clusterName | Remove-Cluster -Confirm:$false
